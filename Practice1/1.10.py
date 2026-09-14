5bfce153a2d02bb15147bf424712a540236521e5
qwertyaaasd <lolkekartemk@gmail.com>
2026-09-15 01:36:50 +0300

    Delete Practice1 directory

--- Practice1/1.10.py
+++
@@ -1,8 +0,0 @@
-from itertools import *
-
-# а)
-alph = 'ТЕИЯРО'
-a = round(1/len(list(permutations(alph, 3))), 5)
-
-# б)
-a = round(1/len(list(permutations(alph, 6))), 5)
--- Practice1/1.8.py
+++
@@ -1,9 +0,0 @@
-import scipy.special as sc
-
-n = 10
-k = 5
-
-# а)
-a = n**k
-# б)
-a = sc.comb(n+k-1, k)
